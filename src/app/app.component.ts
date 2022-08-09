d92cc8fc38ee1f76ef61582fcbd648917485ec3b
Arumi <arumi@thwiki.cc>
2022-08-10 02:37:58 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ba-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'ba-armory';
+}
